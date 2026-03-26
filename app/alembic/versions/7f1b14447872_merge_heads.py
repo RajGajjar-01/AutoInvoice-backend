e46331ac69ba1f9752beb1cc69d9bbd48dda0a72
RajGajjar-01 <irajgajjar.2004@gmail.com>
2026-03-26 23:37:26 +0530

    fix: remove Supabase-specific migrations for fresh Railway deployment

--- app/alembic/versions/7f1b14447872_merge_heads.py
+++ app/alembic/versions/7f1b14447872_merge_heads.py
@@ -1,18 +1,18 @@
 """merge_heads
 
 Revision ID: 7f1b14447872
-Revises: 5a6b7c8d9e0f, migrate_supabase_to_local
+Revises: 5a6b7c8d9e0f
 Create Date: 2026-03-26 23:34:38.222058
 
 """
+
 from alembic import op
 import sqlalchemy as sa
 import sqlmodel.sql.sqltypes
 
 
-# revision identifiers, used by Alembic.
-revision = '7f1b14447872'
-down_revision = ('5a6b7c8d9e0f', 'migrate_supabase_to_local')
+revision = "7f1b14447872"
+down_revision = "5a6b7c8d9e0f"
 branch_labels = None
 depends_on = None
 
